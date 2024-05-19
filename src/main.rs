84f9b5b6f1e352af5ba08e23d3cfc43463841a57
Illusion <1161524405@qq.com>
2024-05-19 11:05:33 +0800

    initialize dotenv and create .env file

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,12 @@ use axum::{extract::MatchedPath, http::Request, response::IntoResponse, routing,
 use tokio::net::TcpListener;
 use tower_http::trace::TraceLayer;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use dotenvy::dotenv;
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
+    
     // initialize logger tracing
     tracing_subscriber::registry()
         .with(tracing_subscriber::fmt::layer())
