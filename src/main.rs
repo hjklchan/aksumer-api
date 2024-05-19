e9e4c7edd72d6b8b0dd098debe424ea09b9ad319
Illusion <1161524405@qq.com>
2024-05-19 10:49:19 +0800

    use logger tracing library

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
-use axum::{response::IntoResponse, routing, Router};
+use axum::{extract::MatchedPath, http::Request, response::IntoResponse, routing, Router};
 use tokio::net::TcpListener;
-
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
-    let app = Router::new().route("/", routing::get(root_handler));
+    let app = Router::new()
+        .route("/", routing::get(root_handler))
+        .layer(TraceLayer::new_for_http());
 
     let tcp_listener = TcpListener::bind("127.0.0.1:8888").await.unwrap();
 
@@ -14,5 +19,7 @@ async fn main() {
 }
 
 async fn root_handler() -> impl IntoResponse {
+    tracing::info!("Access to root_handler!");
+
     "Hello, Askumer-API!"
 }
