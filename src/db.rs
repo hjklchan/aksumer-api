df0401bcea2d24226208fb3dc062bb30ca3e3a71
Illusion <1161524405@qq.com>
2024-05-19 16:03:58 +0800

    sqlx prepare and fmt the code

--- src/db.rs
+++ src/db.rs
@@ -3,11 +3,14 @@ use std::env;
 use sqlx::{mysql::MySqlPoolOptions, types::chrono, MySql, Pool};
 
 /// ## Initialize the MySQL database
-/// 
-/// return the database pool with MySQL 
+///
+/// return the database pool with MySQL
 pub async fn init() -> Pool<MySql> {
     let database_url: String = env::var("DATABASE_URL").unwrap();
-    MySqlPoolOptions::new().connect(&database_url).await.unwrap()
+    MySqlPoolOptions::new()
+        .connect(&database_url)
+        .await
+        .unwrap()
 }
 
 /// ## User model
