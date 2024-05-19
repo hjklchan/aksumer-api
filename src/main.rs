b352b0eaf5da3674beabee64993477fe8c00b744
Illusion <1161524405@qq.com>
2024-05-19 10:34:30 +0800

    make simpl application and server bootstrap

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
+use axum::{response::IntoResponse, routing, Router};
+use tokio::net::TcpListener;
+
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    println!("Hello, world!");
+    let app = Router::new().route("/", routing::get(root_handler));
+
+    let tcp_listener = TcpListener::bind("127.0.0.1:8888").await.unwrap();
+
+    axum::serve(tcp_listener, app).await.unwrap();
+}
+
+async fn root_handler() -> impl IntoResponse {
+    "Hello, Askumer-API!"
 }
