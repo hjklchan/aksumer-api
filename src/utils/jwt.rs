98af936bcb30414b2ff5c4dbb6688fc0d60d0414
Illusion <1161524405@qq.com>
2024-05-20 19:57:54 +0800

    add jsonwebtoken and chrono

--- src/utils/jwt.rs
+++ src/utils/jwt.rs
@@ -1,19 +1,45 @@
-use serde::{Serialize, Deserialize};
+use jsonwebtoken::{errors::Error, DecodingKey, EncodingKey, Header, TokenData, Validation};
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Payload {
-    id: u64,
-    username: String,
+    pub id: u64,
+    pub username: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Claims<T> {
+pub struct Claims {
     pub exp: usize,
     pub iat: usize,
     pub payload: Payload,
-    _unused: std::marker::PhantomData<T>
 }
 
-impl<T> Claims<T> {
-    // TODO
+impl From<Payload> for Claims {
+    fn from(value: Payload) -> Self {
+        Self::new(value.id, value.username)
+    }
+}
+
+impl Claims {
+    fn new(id: u64, username: String) -> Self {
+        Self {
+            exp: (chrono::Local::now() + chrono::Duration::days(30)).timestamp() as usize,
+            iat: chrono::Local::now().timestamp() as usize,
+            payload: Payload { id, username },
+        }
+    }
+}
+
+pub fn generate(secret: &str, payload: Payload) -> Result<String, Error> {
+    let encoding_key = EncodingKey::from_secret(secret.as_ref());
+    Ok(jsonwebtoken::encode(
+        &Header::default(),
+        &(Claims::from(payload)),
+        &encoding_key,
+    )?)
+}
+
+pub fn decode(token: &str, secret: &str) -> Result<TokenData<Claims>, Error> {
+    let decoding_key = DecodingKey::from_secret(secret.as_ref());
+    jsonwebtoken::decode::<Claims>(token, &decoding_key, &Validation::default())
 }
