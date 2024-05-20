9c84bd05c2e769522ae0a77f50def69e7fd9d08c
Lucas chen <lucas.chen@gbo-asia.com>
2024-05-20 15:14:52 +0800

    register new handler into route

--- src/routes.rs
+++ src/routes.rs
@@ -1,19 +1,17 @@
-use crate::handler;
+use crate::{handler, AppState};
 use axum::response::IntoResponse;
 use axum::{routing, Router};
 
 /// ## App routes
 ///
 /// The call path is `Router -> Handler`
-pub fn with_state<S, AS>(state: AS) -> Router<S>
-where
-    AS: Clone + Send + Sync + 'static,
-{
+pub fn with_state<S>(state: AppState) -> Router<S> {
     Router::new()
         // root
         .route("/", routing::get(root_handler))
         // login
         .route("/login", routing::get(handler::user::login_handler))
+        .route("/users", routing::post(handler::user::create_handler))
         .with_state(state)
 }
 
